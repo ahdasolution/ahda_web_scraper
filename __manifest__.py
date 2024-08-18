ebc262bbce9cab6cc3b273f665a489df0d1d2957
infonusa <ariehariady@gmail.com>
2024-08-18 13:24:04 +0700

    fix manifest

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,7 @@
     "author": "AHDA Tech Solution",
     "category": "Uncategorized",
     "version": "13.0.0",
+    "license" : "LGPL-3",
     "depends": ["base", "mail"],
     "summary": """
 This module allows you to scrape a website and do something in odoo with the data""",
